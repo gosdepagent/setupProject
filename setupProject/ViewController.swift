984e38a06084f76d38c4a39e42b258605adde6de
gosdepagent <velikanova6277@gmail.com>
2024-12-20 00:41:55 -0500

    Initial Commit

---
+++ setupProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  setupProject
+//
+//  Created by Yanye Velikanova on 12/20/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
