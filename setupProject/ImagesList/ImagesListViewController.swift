f045e7fc8613e3f5e699958001340eea7fa2ce81
Yana V <velikanova6277@gmail.com>
2024-12-29 23:46:42 -0500

    all done

--- setupProject/ImagesList/ImagesListViewController.swift
+++ setupProject/ImagesList/ImagesListViewController.swift
@@ -77,7 +77,7 @@ extension ImagesListViewController: UITableViewDelegate {
             return 0
         }
         
-        let imageInsets = UIEdgeInsets(top: 4, left: 16, bottom: 4, right: 16)
+         let imageInsets = UIEdgeInsets(top: 4, left: 16, bottom: 4, right: 16)
         let imageViewWidth = tableView.bounds.width - imageInsets.left - imageInsets.right
         let imageWidth = image.size.width
         let scale = imageViewWidth / imageWidth
