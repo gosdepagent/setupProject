17b19dd8f38126050359bd618233f1635546174a
Yana V <velikanova6277@gmail.com>
2025-01-01 11:58:58 -0500

    1 part done

---
+++ setupProject/ProfileViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileViewController.swift
+//  setupProject
+//
+//  Created by Yanye Velikanova on 1/1/25.
+//
+
+import Foundation
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    @IBOutlet weak var Picture: UIImageView!
+    @IBOutlet weak var Exit: UIButton!
+    @IBOutlet weak var Name: UILabel!
+    @IBOutlet weak var Nickname: UILabel!
+    @IBOutlet weak var Text: UILabel!
+    
+    @IBAction private func didTapLogoutButton() {
+    }
+}
